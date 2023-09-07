64acae5c5700065bff15b7c11253d3e69a1b6fcd
Enrico R <enricorrx@gmail.com>
2023-09-07 16:53:17 +0200

    dependencies

---
+++ install-env.sh
@@ -0,0 +1,9 @@
+# environment for eXtreme Gradient Boosting (XGBoost)
+
+pip install --user xgboost
+
+# Use CPU only
+#conda install -c conda-forge py-xgboost-cpu
+
+# Use NVIDIA GPU
+conda install -c conda-forge py-xgboost-gpu
