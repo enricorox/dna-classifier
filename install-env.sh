5a0768181689e45d2ae01425b0246d6915558f50
Enrico R <enricorrx@gmail.com>
2023-09-28 13:59:18 +0200

    use 2.0.0 XGBoost version

--- install-env.sh
+++ install-env.sh
@@ -1,9 +1,12 @@
 # environment for eXtreme Gradient Boosting (XGBoost)
 
-pip install --user xgboost graphviz
+conda create -y -n xgb python==3.8
+conda activate xgb
+
+yes | pip install --yes xgboost==2.0.0 graphviz pandas matplotlib numpy scikit-learn
 
 # Use CPU only
 #conda install -c conda-forge py-xgboost-cpu
 
 # Use NVIDIA GPU
-conda install -c conda-forge py-xgboost-gpu
+# conda install -c conda-forge py-xgboost-gpu
