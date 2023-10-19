e273d2502dabc34e0b168f08440acfa3badbfaf7
Enrico R <enricorrx@gmail.com>
2023-10-19 23:08:11 +0200

    v2.0 can use GPU

--- install-env.sh
+++ install-env.sh
@@ -3,10 +3,4 @@
 conda create -y -n xgb python==3.8
 conda activate xgb
 
-yes | pip install --yes xgboost==2.0.0 graphviz pandas matplotlib numpy scikit-learn
-
-# Use CPU only
-#conda install -c conda-forge py-xgboost-cpu
-
-# Use NVIDIA GPU
-# conda install -c conda-forge py-xgboost-gpu
+yes | pip install xgboost==2.0.0 graphviz pandas matplotlib numpy scikit-learn
